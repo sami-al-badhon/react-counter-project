76e6c8296855d882048f95955f12a8620b427d27
sami-al-badhon <mdsamialbadhon@gmail.com>
2024-09-07 14:11:34 +0600

    counter-project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './style.min.css';
+import EventHandeler from './components/EventHandeler';
+import FunctionalComponent from './components/FunctionalComponent';
+import CounterApp from './components/CounterApp';
+
+// import ConditionalRendering from './components';
+
+function App() {
+  return (
+    <div>
+      <CounterApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CounterApp.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function CounterApp() {
+  const [Count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(Count => Count + 1);
+  };
+  const decrement = () => {
+    setCount(Count => Count - 1);
+  };
+  const zero = () => {
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <h1 className="app-title">Counter App</h1>
+      <div id="counter-div">
+        <h2 className="count">Count : {Count} </h2>
+        <div className="btn-div">
+          <button
+            className="btn"
+            onClick={increment}
+            disabled={Count >= 5 ? true : false}>
+            +
+          </button>
+          <button
+            className="btn"
+            onClick={decrement}
+            disabled={Count <= -5 ? true : false}>
+            -
+          </button>
+          <button className="btn" onClick={zero}>
+            0
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CounterApp;
