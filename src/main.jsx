6e6471450d8355a12c916b3a1efa2c6891cc1034
sami-al-badhon <mdsamialbadhon@gmail.com>
2024-10-09 13:16:01 +0600

    updated version

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
 
+// @ts-ignore
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
